84349f6d403db4aaac73a5f84786cf8803af81af
Oscar <oscarcr189@outlook.com>
2023-08-24 22:58:38 -0600

    creando un flux con observador en reactivo

--- src/main/java/com/examples/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/examples/springboot/webflux/app/controllers/ProductoController.java
@@ -1,5 +1,7 @@
 package com.examples.springboot.webflux.app.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,10 +19,17 @@ public class ProductoController {
 	@Autowired
 	private ProductoDao dao;
 	
+	private static final Logger log = LoggerFactory.getLogger(ProductoController.class);
+	
 	@GetMapping({"/listar","/"})
 	public String listar(Model model) {
 		
-		Flux<Producto> productos = dao.findAll();
+		Flux<Producto> productos = dao.findAll().map(producto -> {
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		});
+		
+		productos.subscribe(prod -> log.info(prod.getNombre()));
 		
 		model.addAttribute("productos", productos);
 		model.addAttribute("titulo","Listado de productos");
