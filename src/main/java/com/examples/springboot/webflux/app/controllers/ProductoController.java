0bf695f2614edb80c3141c1114b06f3a7aa7996c
Oscar <oscarcr189@outlook.com>
2023-08-25 22:13:11 -0600

    creando un flux con observador en reactivo chunked

--- src/main/java/com/examples/springboot/webflux/app/controllers/ProductoController.java
+++ src/main/java/com/examples/springboot/webflux/app/controllers/ProductoController.java
@@ -1,11 +1,14 @@
 package com.examples.springboot.webflux.app.controllers;
 
+import java.time.Duration;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring6.context.webflux.ReactiveDataDriverContextVariable;
 
 import com.examples.springboot.webflux.app.models.dao.ProductoDao;
 import com.examples.springboot.webflux.app.models.documents.Producto;
@@ -35,5 +38,49 @@ public class ProductoController {
 		model.addAttribute("titulo","Listado de productos");
 		
 		return "listar";
+	}
+	
+	@GetMapping("/listar-datadriver")
+	public String listarDataDriver(Model model) {
+		
+		Flux<Producto> productos = dao.findAll().map(producto -> {
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		}).delayElements(Duration.ofSeconds(1));
+		
+		productos.subscribe(prod -> log.info(prod.getNombre()));
+		
+		model.addAttribute("productos",new ReactiveDataDriverContextVariable(productos, 2));
+		model.addAttribute("titulo","Listado de productos");
+		
+		return "listar";
+	}
+	
+	@GetMapping("/listar-full")
+	public String listarFull(Model model) {
+		
+		Flux<Producto> productos = dao.findAll().map(producto -> {
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		}).repeat(5000);
+		
+		model.addAttribute("productos",new ReactiveDataDriverContextVariable(productos, 2));
+		model.addAttribute("titulo","Listado de productos");
+		
+		return "listar";
+	}
+	
+	@GetMapping("/listar-chunked")
+	public String listarChunked(Model model) {
+		
+		Flux<Producto> productos = dao.findAll().map(producto -> {
+			producto.setNombre(producto.getNombre().toUpperCase());
+			return producto;
+		}).repeat(5000);
+		
+		model.addAttribute("productos",new ReactiveDataDriverContextVariable(productos, 2));
+		model.addAttribute("titulo","Listado de productos");
+		
+		return "listar-chunked";
 	}
 }
